c549f4957222bf7ba6561463371484f83bb71ecd
Johan Feliz <johanefelizu@gmail.com>
2024-03-17 00:08:42 -0400

    Semantic structure - Header component - Welcome component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+
+			"accent": "var(--accent)",
+			"accent-2": "var(--accent-2)",
+		}
 	},
 	plugins: [],
 }
